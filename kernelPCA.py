0e34bc62e970a876e2b82970f985522721aa86e2
shimizu_taira <taira17aug@yahoo.co.jp>
2022-12-27 19:34:06 +0900

    主成分分析可視化まで
    主成分分析可視化まで。
    今後クラスラベルを表示させた可視化を実施

--- kernelPCA.py
+++ kernelPCA.py
@@ -2,6 +2,7 @@ import networkx as nx
 import collections
 import copy
 import numpy as np
+from scipy.sparse.linalg import svds
 
 """
 networkxのgraphインスタンスから隣接行列を返す
@@ -9,7 +10,7 @@ G: networkx.Graph
 return : 隣接行列
 """
 def nx_to_adj(G):
-    return np.array(nx.adjacency_matrix(G,weight=None).todense().astype(int))
+    return np.array(nx.adjacency_matrix(G, weight=None).todense().astype(int))
 
 
 """
@@ -39,3 +40,34 @@ def adj_to_lap(g):
     # 次数行列を取得
     matrix_ord = adj_to_ord(g)
     return matrix_ord - g
+
+
+"""
+拡散カーネル。ラプラシアン行列を入力とする
+"""
+def diffusion_kernel(lap, T):
+    # 符号反転
+    lap = -1*lap
+   
+    # パラメータTをかける
+    lap = T*lap
+   
+    return np.exp(lap)
+
+class PCA:
+    def __init__(self, n_components, tol = 0.0, random_seed = 0) -> None:
+        self.n_components = n_components
+        self.tol = tol
+        self.random_state_ = np.random.RandomState(random_seed)
+
+        pass
+    def fit(self, X):
+        v0 = self.random_state_.randn(min(X.shape))
+        xbar = X.mean(axis = 0)
+        Y = X - xbar
+        S = np.dot(Y.T, Y)
+        U, Sigma, VT = svds(S,v0=v0)
+        self.VT_ = VT[::-1, :]
+    
+    def transform(self, X):
+        return self.VT_.dot(X.T).T
